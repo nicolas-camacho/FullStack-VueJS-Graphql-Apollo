9d8c9e56b4f383d211d4e4eda6c78620914ab58d
Nicolas Camacho <ncamacho@cuc.edu.co>
2019-04-20 18:04:51 -0500

    querys learned and feels good

---
+++ src/components/LinkItem.vue
@@ -0,0 +1,12 @@
+<template>
+<div>
+  {{link.description}} {{link.url}}
+</div>
+</template>
+
+<script>
+export default {
+  name: 'LinkItem',
+  props: ['link']
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,15 +1,44 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import {
+  ApolloClient
+} from 'apollo-client'
+import {
+  HttpLink
+} from 'apollo-link-http'
+import {
+  InMemoryCache
+} from 'apollo-cache-inmemory'
+
+import VueApollo from 'vue-apollo'
+
 import Vue from 'vue'
 import App from './App'
-import router from './router'
+import 'tachyons'
 
 Vue.config.productionTip = false
 
+const httpLink = new HttpLink({
+  uri: 'https://api.graph.cool/simple/v1/cjuq0u5hh335l0168ccjt7ubv'
+})
+
+const apolloClient = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+  connectToDevTool: true
+})
+
+Vue.use(VueApollo)
+
+const apolloProvider = new VueApollo({
+  defaultClient: apolloClient,
+  defaultOptions: {
+    $loadingKey: 'loading'
+  }
+})
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
+  provide: apolloProvider.provide(),
+  render: h => h(App)
 })
